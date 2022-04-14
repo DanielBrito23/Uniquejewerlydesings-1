06d53deb7ba5c75fabbf19c312c7286e4deb4e88
Coris12 <corina.guzman.est@tecazuay.edu.ec>
2022-04-14 11:08:47 +0700

    lista Factura
    cambio de la consulta

--- src/uniquejewerlydesings/control/facturaControl.java
+++ src/uniquejewerlydesings/control/facturaControl.java
@@ -368,11 +368,11 @@ public class facturaControl extends validacion {
         valor = Integer.parseInt(vistaFactura.getTxtcuerpo().getText());
         cuerpoDB.setId_cuerpo(valor);
         cuerpoDB.setId_encabezado(Integer.parseInt(vistaFactura.getTxtidfac().getText()));
-        //cuerpoDB.setTotal_reparacion(Double.parseDouble(vistaFactura.getTxtReparacion().getText()));
-        //cuerpoDB.setReparacion(vistaFactura.getTxtreparaciones().getText());
-        //cuerpoDB.setTotal(Double.parseDouble(vistaFactura.getTxtpricetotal().getText()));
-        //cuerpoDB.setAbono(Double.parseDouble(vistaFactura.getTxtAbono().getText()));
-        //cuerpoDB.setValor_pendiente(Double.parseDouble(vistaFactura.getTxtValorPediente().getText()));
+        cuerpoDB.setTotal_reparacion(Double.parseDouble(vistaFactura.getTxtReparacion().getText()));
+        cuerpoDB.setReparacion(vistaFactura.getTxtreparaciones().getText());
+        cuerpoDB.setTotal(Double.parseDouble(vistaFactura.getTxtpricetotal().getText()));
+        cuerpoDB.setAbono(Double.parseDouble(vistaFactura.getTxtAbono().getText()));
+        cuerpoDB.setValor_pendiente(Double.parseDouble(vistaFactura.getTxtValorPediente().getText()));
 //        cuerpoDB.setProducto(vistaFactura.getTxtpricetotal().setText(id_producto));
 
         Iterator<String> iterator = idsProd.iterator();
